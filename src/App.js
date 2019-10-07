76ce1fc21ab79577edd5c0d6443ac7cde9e4fb16
Martin Klauer <mdklauer@gmail.com>
2019-10-07 15:43:22 +0200

    MAJ

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Contact
+       name="Anita Sutton" 
+       avatar="https://media.istockphoto.com/photos/fashion-portrait-of-a-modern-girl-picture-id507838634" 
+       online
+      /> 
+      
+      <Contact
+      name="Clara Sutton" 
+      avatar="https://media.istockphoto.com/photos/fashion-portrait-of-a-modern-girl-picture-id507838634" 
+      
+     /> 
+
+      <Contact
+       name="Françoise Sutton" 
+       avatar="https://media.istockphoto.com/photos/fashion-portrait-of-a-modern-girl-picture-id507838634" 
+       online
+      /> 
     </div>
   );
 }
---
+++ src/components/Contact.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Contact.css'
+
+function Contact(props){
+    return(
+        <div className="Contact">
+            <img src={props.avatar} alt={props.name} className="avatar"></img>
+            <div>
+                <h5 className="name">{props.name}</h5>
+                {props.online ? 
+                <div className='status'><div className='status-online'></div><p className='status-text'>Online</p></div> :
+                <div className='status'><div className='status-offline'></div><p className='status-text'>Offline</p></div> 
+                }
+            </div>
+        </div>
+    )
+}
+
+
+
+
+
+
+
+
+
+export default Contact;
